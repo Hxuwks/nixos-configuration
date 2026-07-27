ab104ee6170467022234c0f8a86e9a357ea958db
Hxuwks <hxuwksdev@gmail.com>
2026-07-28 00:59:51 +0300

    Update hardware.nix

--- hosts/thinkpad/hardware.nix
+++ hosts/thinkpad/hardware.nix
@@ -8,22 +8,22 @@
   zramSwap = {
     enable = true;
     algorithm = "zstd";
-    memoryPercent = 50; # Выделять до 50% ОЗУ под сжатый swap (можно изменить на 60-100% при необходимости)
+    memoryPercent = 50; 
   };
 
-  # Оптимизация работы со swap в ОЗУ (активно используем zram до обращения к файлу/диску)
+  
   boot.kernel.sysctl = {
-    "vm.swappiness" = 180;             # Агрессивнее сжимаем неактивные страницы в zram
-    "vm.watermark_boost_factor" = 0;   # Помогает избегать микрофризов
+    "vm.swappiness" = 180;             
+    "vm.watermark_boost_factor" = 0;  
     "vm.watermark_scale_factor" = 125;
-    "vm.page-cluster" = 0;             # Считываем страницы по одной (оптимально для zram)
+    "vm.page-cluster" = 0;         
   };
 
-  # Инициализация графического ядра amdgpu в раннем initrd
+  
   boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
 
-  # Аппаратное ускорение видео (VA-API / VDPAU для AMD Radeon Graphics)
+  
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
@@ -34,12 +34,12 @@
     ];
   };
 
-  # Оптимизация энергосбережения и батареи 
+   
   services.power-profiles-daemon.enable = true; 
 
-  # Автоматический TRIM для NVMe SSD
+  
   services.fstrim.enable = true;
 
-  # Драйвер сенсорной панели ThinkPad
+  
   services.libinput.enable = true;
 }
