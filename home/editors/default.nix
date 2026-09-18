0299a89492ece0db98b0e79c1ce641885722a80e
Hxuwks <hxuwksdev@gmail.com>
2026-09-18 19:00:56 +0300

    zed changes

--- home/editors/default.nix
+++ home/editors/default.nix
@@ -2,13 +2,12 @@
   home.packages = with pkgs; [
     obsidian
 
-    # LSP, линтеры и форматировщики
-    nil # Nix LSP (которого просит Zed)
-    nixd # Альтернативный Nix LSP
-    statix # Nix linter
-    alejandra # Nix formatter
-    pyright # Python LSP
-    clang-tools # C/C++ (clangd)
+    nil
+    nixd
+    statix
+    alejandra
+    pyright
+    clang-tools
   ];
 
   programs.zed-editor = {
@@ -17,14 +16,12 @@
     userSettings = {
       theme = "Catppuccin Latte";
 
-      # Пробрасываем системные LSP в Zed
       lsp = {
         nil = {binary = {path = "nil";};};
         pyright = {binary = {path = "pyright";};};
         clangd = {binary = {path = "clangd";};};
       };
 
-      # Настройки для языка Nix
       languages = {
         Nix = {
           language_servers = ["nil"];
